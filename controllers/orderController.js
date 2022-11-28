bed96271d18b63055598845bfd3dc1d7bf30db7b
windyandcloudy <phonghv006@gmail.com>
2022-11-29 02:20:51 +0700

    done paypalll

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,178 +1,212 @@
-const ErrorResponse = require('../helpers/ErrorResponse');
-const asyncHandle = require('../middlewares/asyncHandle');
-const Order = require('../models/Order');
-const OrderDetail = require('../models/OrderDetail');
-const User = require('../models/User');
-const Cart = require('../models/Cart');
-const mongoose = require('mongoose');
-const Product = require('../models/Product');
+const ErrorResponse = require("../helpers/ErrorResponse");
+const asyncHandle = require("../middlewares/asyncHandle");
+const Order = require("../models/Order");
+const OrderDetail = require("../models/OrderDetail");
+const User = require("../models/User");
+const Cart = require("../models/Cart");
+const mongoose = require("mongoose");
+const Product = require("../models/Product");
 const sendResponse = require("../helpers/SendResponse");
 
 module.exports = {
+  // @route [POST] /api/order
+  // @desc Add order and order detail
+  // @access Only role user
+  addOrder: asyncHandle(async (req, res, next) => {
+    console.log(req.body);
+    const userId = req.userId;
+    const { address, phoneNumber, carts } = req.body;
 
-    // @route [POST] /api/order
-    // @desc Add order and order detail
-    // @access Only role user
-    addOrder: asyncHandle(async (req, res, next) => {
-        console.log(req.body);
-        const userId = req.userId;
-        const { address, phoneNumber, carts } = req.body;
+    // Simple validation
+    if (!(address && phoneNumber && carts)) {
+      return next(new ErrorResponse(400, "Lack of information"));
+    }
 
-        // Simple validation
-        if(!(address && phoneNumber && carts)) {
-            return next(new ErrorResponse(400, 'Lack of information'));
-        }
+    // Create session
+    const session = await mongoose.startSession();
+    session.startTransaction();
 
-        // Create session
-        const session = await mongoose.startSession();
-        session.startTransaction();
-        
-        try {
-            const options = { session };
+    try {
+      const options = { session };
 
-            const user = await User.findOne({ _id: userId }, null, options);
+      const user = await User.findOne({ _id: userId }, null, options);
 
-            if(!user) {
-                await session.abortTransaction();
-                session.endSession();
+      if (!user) {
+        await session.abortTransaction();
+        session.endSession();
 
-                return next(new ErrorResponse(404, 'User not found'));
-            }
+        return next(new ErrorResponse(404, "User not found"));
+      }
 
-            // Check for existing product in user's cart and delete them
-            const productsInCart = await Promise.all(carts.map(async cartId => {
-                const cart = await Cart.findOneAndDelete({ _id: cartId, user: userId }, options).populate('product');
+      // Check for existing product in user's cart and delete them
+      const productsInCart = await Promise.all(
+        carts.map(async (cartId) => {
+          const cart = await Cart.findOneAndDelete(
+            { _id: cartId, user: userId },
+            options
+          ).populate("product");
 
-                const product = await Product.findById(cart.product._id, null, options);
-                product.sold += cart.quantity;
-                await product.save();
+          const product = await Product.findById(
+            cart.product._id,
+            null,
+            options
+          );
+          product.sold += cart.quantity;
+          await product.save();
 
-                return cart;
-            }));
+          return cart;
+        })
+      );
 
-            const totalAmount = productsInCart.reduce((total, cart) => {
-                const { quantity, product } = cart;
-                const { price, discount } = product;
-                
-                return total + quantity * (price * ((100 - discount)/100));
-            }, 0)
+      const totalAmount = productsInCart.reduce((total, cart) => {
+        const { quantity, product } = cart;
+        const { price, discount } = product;
 
-            // Check user's account balance 
-            if(user.accountBalance < totalAmount) {
-                await session.abortTransaction();
-                session.endSession();
+        return total + quantity * (price * ((100 - discount) / 100));
+      }, 0);
 
-                return next(new ErrorResponse(400, "User's account balance is not enough for payment"));
-            }
+      // Check user's account balance
+      // if(user.accountBalance < totalAmount) {
+      //     await session.abortTransaction();
+      //     session.endSession();
 
-            const order = await Order.create([{
-                user: userId,
-                address,
-                phoneNumber,
-                totalAmount,
-            }], options);
+      //     return next(new ErrorResponse(400, "User's account balance is not enough for payment"));
+      // }
 
-            const newOrder = order[0];
-            
-            user.accountBalance -= totalAmount;
-            await user.save();
+      const order = await Order.create(
+        [
+          {
+            user: userId,
+            address,
+            phoneNumber,
+            totalAmount,
+          },
+        ],
+        options
+      );
 
-            // Transfer money to recipient's account
-            const receiver = await User.findOne({ username: 'badong2001'}, null, options);
-            
-            if(!receiver) {
-                await session.abortTransaction();
-                session.endSession();
+      const newOrder = order[0];
 
-                return next(new ErrorResponse(404, 'Receiver not found'));
-            }
+      // user.accountBalance -= totalAmount;
+      await user.save();
 
-            receiver.accountBalance += totalAmount;
-            await receiver.save();
+      // Transfer money to recipient's account
+      const receiver = await User.findOne(
+        { username: "badong2001" },
+        null,
+        options
+      );
 
-            // Create array detail of order
-            const orderDetailArray = productsInCart.map(cart => {
-                
-                return {
-                    order: newOrder._id,
-                    product: cart.product._id,
-                    quantity: cart.quantity,
-                    discount: cart.product.discount,
-                    price: cart.product.price,
-                };
-            });
+      if (!receiver) {
+        await session.abortTransaction();
+        session.endSession();
 
-            const orderDetails = await OrderDetail.insertMany(orderDetailArray, options);
-            
-            // End session
-            await session.commitTransaction();
-            session.endSession();
+        return next(new ErrorResponse(404, "Receiver not found"));
+      }
 
-            res.json({ success: true, message: 'Add order successfully', order: newOrder, orderDetails });
-        } catch (error) {
-            await session.abortTransaction();
-            session.endSession();
+      receiver.accountBalance += totalAmount;
+      await receiver.save();
 
-            return next(new ErrorResponse(400, error.message));
-        }
-    }),
-
-    // @route [GET] /api/order/user
-    // @desc Get all order of user
-    // @access Only role user
-    getOrderUser: asyncHandle(async (req, res, next) => {
-        const userId = req.userId;
-
-        const orders = await Order.find({ user: userId }).populate({ path: 'orderDetails', populate: 'product'}).populate('user');
-
-        res.json({ success: true, orders });
-    }),
-
-    // @route [GET] /api/order/admin
-    // @desc Get all order in database
-    // @access Only role admin
-    getAllOrders: asyncHandle(async (req, res, next) => {
-        const page = +req.query.page || 1;
-        const limit = +req.query.limit || 10;
-        const startIndex = (page - 1) * limit;
-        const total = await Order.countDocuments();
-        const totalPage = Math.ceil(total / limit);
-        const pagination = {
-        page,
-        limit,
-        total,
-        totalPage,
+      // Create array detail of order
+      const orderDetailArray = productsInCart.map((cart) => {
+        return {
+          order: newOrder._id,
+          product: cart.product._id,
+          quantity: cart.quantity,
+          discount: cart.product.discount,
+          price: cart.product.price,
         };
-        const orders = await Order.find({}).populate({ path: 'orderDetails', populate: 'product' }).populate('user').skip(startIndex)
-        .limit(limit);;
+      });
 
-        return sendResponse(res, "Get list order successfully.", orders, pagination);
-        // res.json({ success: true, orders });
-    }),
+      const orderDetails = await OrderDetail.insertMany(
+        orderDetailArray,
+        options
+      );
 
-    // @route [GET] /api/order/:id
-    // @desc Get order by id
-    // @access Private
-    getOrder: asyncHandle(async (req, res, next) => {
-        const orderId = req.params.id;
+      // End session
+      await session.commitTransaction();
+      session.endSession();
 
-        const order = await Order.findById(orderId).populate({ path: 'orderDetails', populate: 'product' }).populate('user');
+      res.json({
+        success: true,
+        message: "Add order successfully",
+        order: newOrder,
+        orderDetails,
+      });
+    } catch (error) {
+      await session.abortTransaction();
+      session.endSession();
 
-        if(!order) 
-            return next(new ErrorResponse(404, 'Not found order'));
+      return next(new ErrorResponse(400, error.message));
+    }
+  }),
 
-        res.json({ success: true, order });
-    }),
-    updateStatus: asyncHandle(async(req, res, next)=>{
-        let id= req.params.id;
-        let {...body}= req.body
-        let order= await Order.findByIdAndUpdate(id, body, {new: true})
-        return res.status(200).json(order)
-    }),
-    deleteOrder: asyncHandle(async(req, res, next)=>{
-        let id= req.params.id;
-        let order= await Order.findByIdAndDelete(id);
-        return res.status(200).json(order);
-    })
+  // @route [GET] /api/order/user
+  // @desc Get all order of user
+  // @access Only role user
+  getOrderUser: asyncHandle(async (req, res, next) => {
+    const userId = req.userId;
 
+    const orders = await Order.find({ user: userId })
+      .populate({ path: "orderDetails", populate: "product" })
+      .populate("user");
+
+    res.json({ success: true, orders });
+  }),
+
+  // @route [GET] /api/order/admin
+  // @desc Get all order in database
+  // @access Only role admin
+  getAllOrders: asyncHandle(async (req, res, next) => {
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 10;
+    const startIndex = (page - 1) * limit;
+    const total = await Order.countDocuments();
+    const totalPage = Math.ceil(total / limit);
+    const pagination = {
+      page,
+      limit,
+      total,
+      totalPage,
+    };
+    const orders = await Order.find({})
+      .populate({ path: "orderDetails", populate: "product" })
+      .populate("user")
+      .skip(startIndex)
+      .limit(limit);
+
+    return sendResponse(
+      res,
+      "Get list order successfully.",
+      orders,
+      pagination
+    );
+    // res.json({ success: true, orders });
+  }),
+
+  // @route [GET] /api/order/:id
+  // @desc Get order by id
+  // @access Private
+  getOrder: asyncHandle(async (req, res, next) => {
+    const orderId = req.params.id;
+
+    const order = await Order.findById(orderId)
+      .populate({ path: "orderDetails", populate: "product" })
+      .populate("user");
+
+    if (!order) return next(new ErrorResponse(404, "Not found order"));
+
+    res.json({ success: true, order });
+  }),
+  updateStatus: asyncHandle(async (req, res, next) => {
+    let id = req.params.id;
+    let { ...body } = req.body;
+    let order = await Order.findByIdAndUpdate(id, body, { new: true });
+    return res.status(200).json(order);
+  }),
+  deleteOrder: asyncHandle(async (req, res, next) => {
+    let id = req.params.id;
+    let order = await Order.findByIdAndDelete(id);
+    return res.status(200).json(order);
+  }),
 };
--- index.js
+++ index.js
@@ -9,6 +9,20 @@ const paypal = require("paypal-rest-sdk");
 
 const orderModel = require("./models/Order");
 
+const ErrorResponse = require("./helpers/ErrorResponse");
+const asyncHandle = require("./middlewares/asyncHandle");
+const Order = require("./models/Order");
+const OrderDetail = require("./models/OrderDetail");
+const User = require("./models/User");
+const Cart = require("./models/Cart");
+const mongoose = require("mongoose");
+const Product = require("./models/Product");
+
+const { verifyAccessToken } = require("./middlewares/verifyToken");
+const permission = require("./middlewares/permission");
+
+const errorHandle = require("./middlewares/errorHandle");
+
 paypal.configure({
   mode: "sandbox", //sandbox or live
   client_id:
@@ -46,58 +60,193 @@ router(app);
 app.get("/", function (req, res) {
   res.render("index");
 });
+let globalAddress;
+app.post(
+  "/api/pay",
+  verifyAccessToken,
+  permission("user"),
+  asyncHandle(async function (req, res, next) {
+    let bd = req.body;
+    //nhớ tạo order
+    const userId = req.userId;
 
-app.post("/api/pay", function (req, res) {
-  let obj = req.body;
-  let id_order = req.query.id_order;
-  let total =
-    (obj || 0) &&
-    obj.reduce(
-      (pre, curr) => pre + Number(curr.price) * Number(curr.quantity),
-      0
-    );
-  const create_payment_json = {
-    intent: "sale",
-    payer: {
-      payment_method: "paypal",
-    },
-    redirect_urls: {
-      return_url:
-        "http://localhost:5000/api/success?total=" +
-        total +
-        "&id_order=" +
-        id_order,
-      cancel_url: "http://localhost:5000/api/cancel",
-    },
-    transactions: [
-      {
-        item_list: {
-          items: obj,
-        },
-        amount: {
-          currency: "USD",
-          total: total.toString(),
-        },
-        description: "Hat for the best team ever",
+    let id_order;
+    const { address, phoneNumber, carts } = req.body;
+    globalAddress = address;
+    // Simple validation
+    if (!carts) {
+      return next(new ErrorResponse(400, "Lack of information"));
+    }
+
+    // Create session
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+      const options = { session };
+
+      const user = await User.findOne({ _id: userId }, null, options);
+
+      if (!user) {
+        await session.abortTransaction();
+        session.endSession();
+
+        return next(new ErrorResponse(404, "User not found"));
+      }
+
+      // Check for existing product in user's cart and delete them
+      const productsInCart = await Promise.all(
+        carts.map(async (cartId) => {
+          const cart = await Cart.findOneAndDelete(
+            { _id: cartId, user: userId },
+            options
+          ).populate("product");
+
+          const product = await Product.findById(
+            cart.product._id,
+            null,
+            options
+          );
+          product.sold += cart.quantity;
+          await product.save();
+
+          return cart;
+        })
+      );
+
+      const totalAmount = productsInCart.reduce((total, cart) => {
+        const { quantity, product } = cart;
+        const { price, discount } = product;
+
+        return total + quantity * (price * ((100 - discount) / 100));
+      }, 0);
+
+      // Check user's account balance
+      // if(user.accountBalance < totalAmount) {
+      //     await session.abortTransaction();
+      //     session.endSession();
+
+      //     return next(new ErrorResponse(400, "User's account balance is not enough for payment"));
+      // }
+      console.log("taoj order");
+      const order = await Order.create(
+        [
+          {
+            user: userId,
+            address: address || "HN",
+            phoneNumber: phoneNumber || "0983154433",
+            totalAmount,
+          },
+        ],
+        options
+      );
+
+      const newOrder = order[0];
+      id_order = order[0]._id;
+      // user.accountBalance -= totalAmount;
+      await user.save();
+
+      // Transfer money to recipient's account
+      const receiver = await User.findOne(
+        { username: "badong2001" },
+        null,
+        options
+      );
+
+      if (!receiver) {
+        await session.abortTransaction();
+        session.endSession();
+
+        return next(new ErrorResponse(404, "Receiver not found"));
+      }
+
+      receiver.accountBalance += totalAmount;
+      await receiver.save();
+
+      // Create array detail of order
+      const orderDetailArray = productsInCart.map((cart) => {
+        return {
+          order: newOrder._id,
+          product: cart.product._id,
+          quantity: cart.quantity,
+          discount: cart.product.discount,
+          price: cart.product.price,
+        };
+      });
+
+      const orderDetails = await OrderDetail.insertMany(
+        orderDetailArray,
+        options
+      );
+
+      // End session
+      await session.commitTransaction();
+      session.endSession();
+    } catch (error) {
+      await session.abortTransaction();
+      session.endSession();
+
+      return next(new ErrorResponse(400, error.message));
+    }
+
+    console.log(id_order);
+    //tạo order
+    console.log("taoj xong order: " + id_order);
+    let obj = bd.carts.map((v) => ({
+      name: v.product.name,
+      quantity: v.quantity,
+      price: v.product.price,
+      currency: "USD",
+    }));
+    console.log("obj: ");
+    console.log(obj);
+
+    let total =
+      (obj || 0) &&
+      obj.reduce(
+        (pre, curr) => pre + Number(curr.price) * Number(curr.quantity),
+        0
+      );
+    console.log("total: " + total);
+    const create_payment_json = {
+      intent: "sale",
+      payer: {
+        payment_method: "paypal",
       },
-    ],
-  };
+      redirect_urls: {
+        return_url: `http://localhost:5000/api/success?total=${total}&id_order=${id_order}`,
+        cancel_url: "http://localhost:5000/api/cancel",
+      },
+      transactions: [
+        {
+          item_list: {
+            items: obj,
+          },
+          amount: {
+            currency: "USD",
+            total: total.toString(),
+          },
+          description: "Hat for the best team ever",
+        },
+      ],
+    };
 
-  paypal.payment.create(create_payment_json, function (error, payment) {
-    if (error) {
-      console.log("Error pay: " + error);
-      res.render("cancle");
-    } else {
-      for (let i = 0; i < payment.links.length; i++) {
-        if (payment.links[i].rel === "approval_url") {
-          console.log("di qua 83");
-          // res.redirect(payment.links[i].href);
-          res.json(payment);
+    paypal.payment.create(create_payment_json, function (error, payment) {
+      if (error) {
+        console.log("Error pay: " + error);
+        res.render("cancle");
+      } else {
+        for (let i = 0; i < payment.links.length; i++) {
+          if (payment.links[i].rel === "approval_url") {
+            // res.redirect(payment.links[i].href);
+            res.json(payment);
+          }
         }
       }
-    }
-  });
-});
+    });
+  })
+);
+
 app.get("/api/cancle", function (req, res) {
   res.render("cancle");
 });
@@ -107,9 +256,6 @@ app.get("/api/success", (req, res) => {
   const total = req.query.total;
   const id_order = req.query.id_order;
 
-  console.log(payerId);
-  console.log(paymentId);
-  console.log(total);
   const execute_payment_json = {
     payer_id: payerId,
     transactions: [
@@ -143,20 +289,23 @@ app.get("/api/success", (req, res) => {
           shipping_address.state;
 
         let bd = {
-          address: address,
+          address: globalAddress || address,
           payment_method: "paypal",
         };
 
         let newOrder = await orderModel.findByIdAndUpdate(id_order, bd, {
           new: true,
         });
-
-        res.render("success");
+        res.json({
+          payment,
+          order: newOrder,
+        });
+        // res.render("success");
       }
     }
   );
 });
-
+app.use(errorHandle);
 app.listen(PORT, () => {
   console.log(`Server running at ${PORT}`);
 });
