8e5c61dbfe7af4460bf643eeb541fa0bb50e2c9f
windyandcloudy <phonghv006@gmail.com>
2022-11-29 22:19:08 +0700

    add quantity product

--- models/Product.js
+++ models/Product.js
@@ -1,50 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ProductSchema = new Schema({
+const ProductSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true,
-    }, 
+      type: String,
+      required: true,
+    },
     thumb: {
-        type: [String],
-        required: true,
-    }, 
+      type: [String],
+      required: true,
+    },
     listImage: {
-        type: [String],
-        default: undefined,
+      type: [String],
+      default: undefined,
     },
     price: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     discount: {
-        type: Number,
-        default: 0,
-    }, 
+      type: Number,
+      default: 0,
+    },
     evaluation: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     description: {
-        type: String,
-        default: '',
+      type: String,
+      default: "",
     },
     sold: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     category: {
-        type: Schema.Types.ObjectId,
-        ref: 'categories',
-        required: true,
+      type: Schema.Types.ObjectId,
+      ref: "categories",
+      required: true,
     },
     deleted: {
-        type: Number,
-        default: 0
-    }
-}, {
+      type: Number,
+      default: 0,
+    },
+    quantity: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-module.exports = mongoose.model('products', ProductSchema);
+module.exports = mongoose.model("products", ProductSchema);
